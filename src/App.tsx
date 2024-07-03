f38da86027edeee6751476ce7eb9dcb87887fe39
solidados <geekbrains.pk@internet.ru>
2024-07-03 16:10:02 +0400

    feat: add components Main and Footer; add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Header from './components/header';
+import Main from './components/main';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <>
+    <div className="wrapper">
       <Header />
-    </>
+      <Main />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/main/index.tsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import './style.scss';
+
+class Main extends Component {
+  render() {
+    return (
+      <main className="main">
+        <div className="main-container">
+          <h2>Main</h2>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default Main;
