f143c79bd27c195a7ee957cf795581b32cab7516
solidados <geekbrains.pk@internet.ru>
2024-07-04 22:02:45 +0400

    feat: wrap App with ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import Header from './components/header';
-import Main from './components/main';
-import Footer from './components/footer';
+import { Component, createRef, RefObject } from 'react';
+import { Header, Main, Footer } from './components';
+import { ErrorBoundary } from './components';
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  mainRef: RefObject<Main>;
+
+  constructor(props: NonNullable<unknown>) {
+    super(props);
+    this.mainRef = createRef();
+  }
+
+  handleSearch = (search: string): void => {
+    if (this.mainRef.current) {
+      this.mainRef.current.fetchMovies(search);
+    }
+  };
+
+  render() {
+    return (
+      <ErrorBoundary>
+        <div className="wrapper">
+          <Header onSearch={this.handleSearch} />
+          <Main ref={this.mainRef} />
+          <Footer />
+        </div>
+      </ErrorBoundary>
+    );
+  }
 }
 
 export default App;
