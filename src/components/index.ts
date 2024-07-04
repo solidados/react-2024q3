e4f8a9f682765573251ebb73b52537e61fe71ab1
solidados <geekbrains.pk@internet.ru>
2024-07-04 22:01:50 +0400

    chore: add export index for Header, Main, Footer, ErrorBoundary components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from './header';
+export { default as Main } from './main';
+export { default as Footer } from './footer';
+export { default as ErrorBoundary } from './errorBoundary';
