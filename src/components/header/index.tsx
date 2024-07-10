f4894d1058572641358ed67f85b16e183c375d2d
solidados <geekbrains.pk@internet.ru>
2024-07-11 00:57:31 +0400

    refactor: Header class -> function component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
 import logo from '/logo-movie.png';
 import './style.scss';
 
@@ -6,73 +6,58 @@ interface HeaderProps {
   onSearch: (search: string) => void;
 }
 
-interface HeaderState {
-  searchInput: string;
-  hasError: boolean;
-}
+const Header: FC<HeaderProps> = ({ onSearch }) => {
+  const [searchInput, setSearchInput] = useState<string>('');
+  const [hasError, setHasError] = useState<boolean>(false);
 
-class Header extends Component<HeaderProps, HeaderState> {
-  constructor(props: HeaderProps) {
-    super(props);
-    this.state = {
-      searchInput: '',
-      hasError: false,
-    };
-  }
-
-  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    this.setState({ searchInput: e.target.value });
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setSearchInput(event.target.value);
   };
 
-  handleSearch = (): void => {
-    const trimmedSearchValue: string = this.state.searchInput.trim();
+  const handleSearch = (): void => {
+    const trimmedSearchValue: string = searchInput.trim();
     localStorage.setItem('movie', trimmedSearchValue);
-    this.props.onSearch(trimmedSearchValue);
+    onSearch(trimmedSearchValue);
   };
-
-  handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>): void => {
+  const handleKeyDown = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ): void => {
     if (event.key === 'Enter') {
-      this.handleSearch();
+      handleSearch();
     }
   };
 
-  throwError = (): void => {
-    this.setState({ hasError: true });
+  const throwError = (): void => {
+    setHasError(true);
   };
 
-  render() {
-    // TODO: remove when not needed
-    if (this.state.hasError) {
-      throw new Error('Test error from Header');
-    }
-
-    return (
-      <header className="header">
-        <div className="header-container">
-          <img src={logo} className="header-logo" alt="logo" />
-          <button className="error-button" onClick={this.throwError}>
-            Throw Error
-          </button>
-          <div className="header-search-bar">
-            <input
-              type="text"
-              className={'header-search-input'}
-              placeholder={'Search...'}
-              value={this.state.searchInput}
-              onChange={this.handleInputChange}
-              onKeyDown={this.handleKeyDown}
-            />
-            <button
-              className="header-search-button"
-              onClick={this.handleSearch}
-            >
-              Search
-            </button>
-          </div>
-        </div>
-      </header>
-    );
+  if (hasError) {
+    throw new Error('Test Error from Header');
   }
-}
+
+  return (
+    <header className="header">
+      <div className="header-container">
+        <img src={logo} className="header-logo" alt="logo" />
+        <button className="error-button" onClick={throwError}>
+          Throw Error
+        </button>
+        <div className="header-search-bar">
+          <input
+            type="text"
+            className={'header-search-input'}
+            placeholder={'Search...'}
+            value={searchInput}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+          />
+          <button className="header-search-button" onClick={handleSearch}>
+            Search
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
