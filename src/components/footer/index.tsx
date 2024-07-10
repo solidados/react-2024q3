9393518f40ab4e42ce83a003043453e326c748da
solidados <geekbrains.pk@internet.ru>
2024-07-11 00:59:47 +0400

    refactor: Footer class -> function component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,19 +1,16 @@
-import { Component } from 'react';
 import logoGh from '/logo-github.png';
 import logoRs from '/logo-rsschool.svg';
 import './style.scss';
 
-class Footer extends Component {
-  render() {
-    return (
-      <header className="footer">
-        <div className="footer-container">
-          <img src={logoGh} alt="logo-gh" className="footer-logo" />
-          <img src={logoRs} alt="logo-rs" className="footer-logo" />
-        </div>
-      </header>
-    );
-  }
-}
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <img src={logoGh} alt="logo-gh" className="footer-logo" />
+        <img src={logoRs} alt="logo-rs" className="footer-logo" />
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
