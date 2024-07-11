739d9e65b0e3965d0f5deafff055f6d3326e45f9
solidados <geekbrains.pk@internet.ru>
2024-07-11 17:23:03 +0400

    feat: add ReactRouter to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import { FC, useState } from 'react';
 import { Footer, Header, Main } from './components';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import ErrorComponent from './components/errorComponent';
 
 const App: FC = () => {
   const [search, setSearch] = useState<string>(
     localStorage.getItem('movie') || 'Star Trek'
   );
+  const [error, setError] = useState<Error | null>(null);
 
   const handleSearch = (search: string): void => {
     setSearch(search);
   };
+
+  const handleReload = (): void => {
+    setError(null);
+  };
+
   return (
-    <div className="wrapper">
-      <Header onSearch={handleSearch} />
-      <Main search={search} />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="wrapper">
+        <Header onSearch={handleSearch} />
+        <main>
+          <Routes>
+            <Route path={'/'} element={<Main search={search} />} />
+            <Route path={'*'} element={<Navigate to={'/'} replace />} />
+          </Routes>
+        </main>
+        {error && <ErrorComponent error={error} onReload={handleReload} />}
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 };
 
