6160e7bdc48eabd29420d4ae05db20bcdfc5c3a2
solidados <geekbrains.pk@internet.ru>
2024-07-11 09:50:42 +0400

    refactor: MovieCard class -> function component

--- src/components/main/ui/MovieCard.tsx
+++ src/components/main/ui/MovieCard.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import { Movie } from '../../../services/types/api.interface';
 
 import './style.scss';
@@ -7,28 +7,23 @@ interface MovieCardProps {
   movie: Movie;
 }
 
-class MovieCard extends Component<MovieCardProps> {
-  render() {
-    const { movie } = this.props;
-    const defaultPoster = '/logo-no-image.png';
+const MovieCard: FC<MovieCardProps> = ({ movie }) => {
+  const defaultPoster = '/logo-no-image.png';
 
-    return (
-      <div key={movie.imdbID} className="movie-card">
-        <img
-          src={
-            movie.Poster && movie.Poster !== 'N/A'
-              ? movie.Poster
-              : defaultPoster
-          }
-          alt={movie.Title}
-        />
-        <div className="movie-card-descr">
-          <h3>{movie.Title}</h3>
-          <p>{movie.Year}</p>
-        </div>
+  return (
+    <div key={movie.imdbID} className="movie-card">
+      <img
+        src={
+          movie.Poster && movie.Poster !== 'N/A' ? movie.Poster : defaultPoster
+        }
+        alt={movie.Title}
+      />
+      <div className="movie-card-descr">
+        <h3>{movie.Title}</h3>
+        <p>{movie.Year}</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default MovieCard;
