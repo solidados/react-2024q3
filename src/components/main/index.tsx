26e9d7f22bf3d87956486199b86fc6f3dcc2d568
solidados <geekbrains.pk@internet.ru>
2024-07-11 10:15:32 +0400

    refactor: Main class -> function component

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { ApiResponse, Movie } from '../../services/types/api.interface';
 import { api } from '../../services/apiClient';
 import { CustomError } from '../../services/errorHandler';
@@ -11,56 +11,49 @@ interface MainState {
   error: CustomError | null;
 }
 
-class Main extends Component<NonNullable<unknown>, MainState> {
-  constructor(props: NonNullable<unknown>) {
-    super(props);
-    this.state = {
-      movies: [],
-      error: null,
-    };
-  }
+const Main: FC<MainState> = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [error, setError] = useState<CustomError | null>(null);
 
-  componentDidMount(): void {
-    const search: string = localStorage.getItem('movie') || 'Star Trek';
-    this.fetchMovies(search).catch((error): Promise<void> => {
-      throw new CustomError(error);
-    });
-  }
-
-  fetchMovies = async (search: string): Promise<void> => {
+  const fetchMovies = async (search: string): Promise<void> => {
     try {
       const data: ApiResponse | undefined = await api.getMovies(search);
 
       if (data && data.Search) {
-        this.setState({ movies: data.Search, error: null });
+        setMovies(data.Search);
+        setError(null);
       } else {
-        this.setState({ movies: [], error: null });
+        setMovies([]);
+        setError(null);
       }
     } catch (error: unknown) {
-      this.setState({ error: error as CustomError });
+      setError(error as CustomError);
     }
   };
 
-  render() {
-    const { movies, error } = this.state;
+  useEffect((): void => {
+    const search: string = localStorage.getItem('movie') || 'Star Trek';
+    fetchMovies(search).catch((error): void => {
+      setError(new CustomError(error));
+    });
+  }, []);
 
-    return (
-      <main className="main">
-        <div className="main-container">
-          {error && <p>{error.message}</p>}
-          {movies && movies.length > 0 ? (
-            <div className="main-result">
-              {movies.map((movie: Movie) => (
-                <MovieCard key={movie.imdbID} movie={movie} />
-              ))}
-            </div>
-          ) : (
-            <p>No movies found</p>
-          )}
-        </div>
-      </main>
-    );
-  }
-}
+  return (
+    <main className="main">
+      <div className="main-container">
+        {error && <p>{error.message}</p>}
+        {movies && movies.length > 0 ? (
+          <div className="main-result">
+            {movies.map((movie: Movie) => (
+              <MovieCard key={movie.imdbID} movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <p>No movies found</p>
+        )}
+      </div>
+    </main>
+  );
+};
 
 export default Main;
