04bda8082f063f78c84b0628e8353ece6d34c1e6
solidados <geekbrains.pk@internet.ru>
2024-07-14 03:37:10 +0400

    feat: movie details in a side bar opens

--- src/components/detailedMovie/index.tsx
+++ src/components/detailedMovie/index.tsx
@@ -39,54 +39,60 @@ const DetailedMovie = () => {
     return <Loader />;
   }
 
-  if (!movie) {
-    return <p>Movie not found</p>;
-  }
-
   return (
-    <div className="detailed-overlay">
-      <div className="detailed-movie">
-        <div className="detailed-movie-poster">
-          <img
-            src={
-              movie.Poster && movie.Poster !== 'N/A'
-                ? movie.Poster
-                : defaultPoster
-            }
-            alt={movie.Title}
-          />
+    <>
+      {movie ? (
+        <div className="detailed-overlay">
+          <div className="detailed-movie">
+            <div className="detailed-movie-poster">
+              <img
+                src={
+                  movie.Poster && movie.Poster !== 'N/A'
+                    ? movie.Poster
+                    : defaultPoster
+                }
+                alt={movie.Title}
+              />
+            </div>
+            <div className="detailed-movie-info">
+              <h2>
+                <strong>Name: </strong>
+                {movie.Title}
+              </h2>
+              <p>
+                <strong>Year:</strong> {movie.Year}
+              </p>
+              <p>
+                <strong>Genre:</strong> {movie.Genre}
+              </p>
+              <p>
+                <strong>Rated:</strong> {movie.Rated}
+              </p>
+              <p className="detailed-movie-plot">
+                <strong>Plot:</strong> {movie.Plot}
+              </p>
+              <p>
+                <strong>Country:</strong> {movie.Country}
+              </p>
+              {movie.Website && (
+                <p>
+                  <strong>Website:</strong>{' '}
+                  <a
+                    href={movie.Website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    review
+                  </a>
+                </p>
+              )}
+            </div>
+          </div>
         </div>
-        <div className="detailed-movie-info">
-          <h2>
-            <strong>Name: </strong>
-            {movie.Title}
-          </h2>
-          <p>
-            <strong>Year:</strong> {movie.Year}
-          </p>
-          <p>
-            <strong>Genre:</strong> {movie.Genre}
-          </p>
-          <p>
-            <strong>Rated:</strong> {movie.Rated}
-          </p>
-          <p className="detailed-movie-plot">
-            <strong>Plot:</strong> {movie.Plot}
-          </p>
-          <p>
-            <strong>Country:</strong> {movie.Country}
-          </p>
-          {movie.Website && (
-            <p>
-              <strong>Website:</strong>{' '}
-              <a href={movie.Website} target="_blank" rel="noopener noreferrer">
-                review
-              </a>
-            </p>
-          )}
-        </div>
-      </div>
-    </div>
+      ) : (
+        <p>Movie not found</p>
+      )}
+    </>
   );
 };
 
