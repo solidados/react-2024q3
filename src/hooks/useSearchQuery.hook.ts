1efa173c4158df7a3850933df542e7067555564d
solidados <geekbrains.pk@internet.ru>
2024-07-13 15:55:55 +0400

    fix: update custom hook to keep full search value on unmounting

--- src/hooks/useSearchQuery.hook.ts
+++ src/hooks/useSearchQuery.hook.ts
@@ -1,14 +1,16 @@
 import { useEffect, useState } from 'react';
 
 export const useSearchQuery = (defaultValue: string) => {
-  const [searchQuery, setSearchQuery] = useState<string>(() => {
+  const [searchQuery, setSearchQuery] = useState<string>((): string => {
     const storedQuery = localStorage.getItem('movie');
     return storedQuery ? storedQuery : defaultValue;
   });
 
   useEffect(() => {
+    localStorage.setItem('movie', searchQuery);
+
     return () => {
-      localStorage.setItem('movie', searchQuery);
+      localStorage.removeItem('movie');
     };
   }, [searchQuery]);
 
