a8925205e0b5cb7052191ad18e1e22dc4d4c648c
solidados <geekbrains.pk@internet.ru>
2024-07-26 15:15:01 +0400

    chore: trying to avoid double fetch requests on MovieCard click

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import MovieList from './ui/MovieList';
 
@@ -17,6 +17,15 @@ const Main: FC<MainProps> = ({ search, page }) => {
     navigate(-1);
     // navigate(`/?s=${search}&page=${page}`);
   };
+
+  useEffect((): void => {
+    if (!location.pathname.includes('details')) {
+      const query = new URLSearchParams(location.search).get('s') || search;
+      const pageParam = new URLSearchParams(location.search).get('page');
+      const pageNumber = pageParam ? parseInt(pageParam, 10) : page;
+      navigate(`/?s=${query}&page=${pageNumber}`);
+    }
+  }, [search, page, location.pathname, navigate, location.search]);
 
   return (
     <main className="main">
